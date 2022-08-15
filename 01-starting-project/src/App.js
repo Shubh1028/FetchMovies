3d80a13efd87555b4422976e1ad565f436ed2416
Shubh <shubh410shekhar@gmail.com>
2022-08-15 14:47:12 +0530

    done

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
   // const dummyMovies = [
@@ -18,36 +18,61 @@ function App() {
   //     releaseDate: '2021-05-19',
   //   },
   // ];
-  const [movies, fetchMovies] = useState([])
-  const [loader, isLoading] = useState(false)
-  async function  fetchMovieHandler(){
+  const [movies, fetchMovies] = useState([]);
+  const [loader, isLoading] = useState(false);
+  const [error, isError] = useState(null);
+  async function fetchMovieHandler() {
     isLoading(true);
+    isError(null)
 
-  const response =  await fetch('https://swapi.dev/api/films/')
-  
-
-  const data = await response.json()
-
-      const tranformMovies = data.results.map((movieData)=>{
+    try {
+      const response = await fetch("https://swapi.dev/api/film/");
+      if (!response.ok) {
+        throw new Error("Something Went Wrong... Retrying");
+      }
+      const data = await response.json();
+      const tranformMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
           title: movieData.title,
           openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-    });
-    fetchMovies(tranformMovies)
-    isLoading(false)
+          releaseDate: movieData.release_date,
+        };
+      });
+      fetchMovies(tranformMovies);
+    } catch(error) {
+      isError(error.message)
+
+     setInterval(fetchMovieHandler, 5000);
+    }
+    isLoading(false);
+
+
+    // const data = await response.json()
+
+    //     const tranformMovies = data.results.map((movieData)=>{
+    //       return {
+    //         id: movieData.episode_id,
+    //         title: movieData.title,
+    //         openingText: movieData.opening_crawl,
+    //         releaseDate: movieData.release_date
+    //       }
+    //   });
+    //   fetchMovies(tranformMovies)
+    //   isLoading(false)
   }
+ 
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
+
       </section>
       <section>
-       {!loader && <MoviesList movies={movies} />}
-       {loader && <p>Loading... Please Wait..</p>}
+        {!loader && <MoviesList movies={movies} />}
+        {loader && !error && <p>Loading... Please Wait..</p>}
+        {!loader && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
